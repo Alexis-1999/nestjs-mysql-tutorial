a1dd792f5740a1146548cabee7da5f9ca63d1c3f
Alexis-1999 <alexisaguilarg19@gmail.com>
2024-01-26 21:18:23 -0300

    Configurar_Entidad_Users

---
+++ Server/src/users/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({ name: 'users'})
+export class User {
+
+    @PrimaryGeneratedColumn()//Crea el id solo
+    id: number
+
+    @Column()//Esto es para que se tranforme en una tabla
+    username: string
+
+    @Column()
+    password: string
+
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP'})
+    createdAt: Date
+
+    @Column({nullable: true})//nullable: true es como decir que no es necesario que cargemos sino que es opcional
+    authStrategy: string
+}
